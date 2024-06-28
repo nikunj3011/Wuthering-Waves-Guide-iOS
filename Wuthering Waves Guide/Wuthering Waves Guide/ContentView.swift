69966e373845b59b6f38827f57aa3410d3f0b22b
nikunj3011 <nikunjrathod3011@gmail.com>
2024-06-27 21:55:38 -0300

    hello world xD

---
+++ Wuthering Waves Guide/Wuthering Waves Guide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wuthering Waves Guide
+//
+//  Created by Nikunj Rathod on 2024-06-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!2")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wuthering Waves Guide/Wuthering Waves Guide/Wuthering_Waves_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wuthering_Waves_GuideApp.swift
+//  Wuthering Waves Guide
+//
+//  Created by Nikunj Rathod on 2024-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Wuthering_Waves_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
